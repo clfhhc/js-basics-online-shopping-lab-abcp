671b368e726a12e2ecd044304bf84d8f13545713
David Chen <clfhhc@gmail.com>
2018-01-15 06:58:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var itemPair={[item]:math.floor(math.random()*100)}
- cart.push(itemPair)
+ var itemPair={[item]:math.floor(math.random()*100)};
+ cart.push(itemPair);
 }
 
 function viewCart() {
