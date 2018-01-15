f6ad3416a78e51973845cfcb38c6fd750c83c8d6
David Chen <clfhhc@gmail.com>
2018-01-15 07:58:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
-    console.log(``)
+    console.log(`Your total cost is ${total()}, which will be charged to the card 83296759.`)
   }
 }
